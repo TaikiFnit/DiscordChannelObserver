1fd38d81fc4aa02a2ef90cb5b065b110d49cf738
TaikiFnit <taikifnitspg@gmail.com>
2018-07-30 15:32:43 +0900

    release v1.0

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  device: {
+    name: 'Bryce',
+    ip: '192.168.0.1',
+  },
+  language: 'ja',
+  bot: {
+    token: 'NDczMDAyNTg5NTA4NzMwODgx.Dj7rDw.qAObhPqNHU3Mh2iJbGvDeQXVD-s'
+  }
+}
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const Discord = require('discord.js');
+const googlehome =  require('google-home-notifier');
+const config= require('./config');
 const client = new Discord.Client();
+
+googlehome.device(config.device.name, config.language);
+googlehome.ip(config.device.ip, config.language);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('voiceStateUpdate', (oldState, newState) => {
+  const channelId = 
+    newState.voiceChannelID === null
+      ? oldState.voiceChannelID
+      : newState.voiceChannelID;
+  const userName = newState.user.username;
+  const channelName = client.channels.find('id', channelId).name;
+
+  let msg = "";
+
+  if (oldState.voiceChannelID == null && newState.voiceChannelID != null) {
+    msg = `${userName}が${channelName}チャンネルにJoinしました`;
+  } else if (oldState.voiceChannelID != null && newState.voiceChannelID == null) {
+    msg = `${userName}が${channelName}チャンネルをLeaveしました`;
+  } else if (oldState.voiceChannelID !== newState.voiceChannelID){
+    msg = `${userName}が${channelName}チャンネルにMoveしました`;
+  } else {
+    return;
   }
+
+  googlehome.notify(msg, function(res) {});
+  console.log(msg)
 });
 
-client.login('NDczMDAyNTg5NTA4NzMwODgx.Dj7rDw.qAObhPqNHU3Mh2iJbGvDeQXVD-s');
+client.login(config.bot.token);
